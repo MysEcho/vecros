d3620549be985a8ffc6093f7c497303348a82910
MysEcho <antareepsinha12@gmail.com>
2025-01-31 01:10:09 +0530

    added dummy run

--- Assignment_1/run.py
+++ Assignment_1/run.py
@@ -6,8 +6,8 @@ bounds = (100, 100, 100)
 velocity = 1.0
 
 requests = [
-    PathRequest(Point3D(0, 0, 0), Point3D(100, 100, 100), start_time=0.0),
-    PathRequest(Point3D(100, 0, 0), Point3D(0, 100, 100), start_time=0.0),
+    PathRequest(Point3D(14, 23, 65), Point3D(67, 32, 73), start_time=0.0),
+    PathRequest(Point3D(91, 79, 43), Point3D(18, 24, 91), start_time=0.0),
 ]
 
 paths = plan_multiple_paths(requests, bounds, velocity)
